4b396275e37ff071725515b74f1a171e5b67a40c
Mark Matney <mmatney@library.ucla.edu>
2015-12-07 12:52:47 -0800

    add ie fonts, fix cross-browser style inconsistencies, disable broken navbar js, update main page content

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,36 +1,44 @@
-$("#about").mouseenter(function() {
-	 $("#sub-navbar").css("visibility", "visible");
-	 
-}).mouseleave(function() {
-	var sub_navbar = $("#sub-navbar");
+$(document).ready(function() {
+/*
+	$("#about").mouseover(function() {
+		console.log(1111);
+		$("#sub-navbar").css("visibility", "visible");
+		 
+	}).mouseout(function() {
+		console.log(2222);
+		var sub_navbar = $("#sub-navbar");
+	
+		if (sub_navbar.is(":hover") || $("#main-navbar").is(":hover")) {
+			return;
+		}
+		console.log("hide1");
+		sub_navbar.css("visibility", "hidden");
+	});
+	
+	$("#sub-navbar").mouseleave(function() {
+		console.log(3333);
+		if ($("#about").is(":hover")) {
+			return;
+		}
+		console.log("hide2");
+		$(this).css("visibility", "hidden");
+	});
+	
+	$("#main-navbar").mouseleave(function() {
+		console.log(4444);
+		if ($("#sub-navbar").is(":hover") || true) {
+			return;
+		}
+		console.log("hide3");
+		$("#sub-navbar").css("visibility", "hidden");
+	});
+*/	
+	$("a#show-login").on("click", function() {
+		$("#login-form").css("visibility", "visible");
+	});
+	
+	$("button#hide-login").on("click", function() {
+		$("#login-form").css("visibility", "hidden");
+	});
 
-	if (sub_navbar.is(":hover") || $("#main-navbar").is(":hover")) {
-		return;
-	}
-	console.log("hide1");
-	sub_navbar.css("visibility", "hidden");
-});
-
-$("#sub-navbar").mouseleave(function() {
-	if ($("#about").is(":hover")) {
-		return;
-	}
-	console.log("hide2");
-	$(this).css("visibility", "hidden");
-});
-
-$("#main-navbar").mouseleave(function() {
-	if ($("#sub-navbar").is(":hover")) {
-		return;
-	}
-	console.log("hide3");
-	$("#sub-navbar").css("visibility", "hidden");
-});
-
-$("a#show-login").on("click", function() {
-	$("#login-form").css("visibility", "visible");
-});
-
-$("button#hide-login").on("click", function() {
-	$("#login-form").css("visibility", "hidden");
 });
