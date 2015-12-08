e557ab5718edf0e571e260f6293e67eb05802ef3
Mark Matney <mmatney@library.ucla.edu>
2015-12-08 15:11:26 -0800

    remove twitter.js; disable facebook login button

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -11,37 +11,15 @@ function login(site) {
 
       xhr.onload = function() {
         if (this.responseText == 'success') {
-          /*
-          var loginLink = document.getElementById('login');
-
-          if (loginLink !== null) {
-            loginLink.text = "Logout";
-            loginLink.setAttribute('href', '/logout');
-          }
-          */
 
           // Let's give more of a visual representation of being logged in
           if (document.getElementById(site + '-login-response') === null) {
             hello(site).api('me').then(function(json) {
 
-              var loginTd = document.getElementById(site + '-login');
-              var userImage = document.createElement('img');
-              var userName = document.createTextNode(' ' + json.name);
-              var divElement = document.createElement('div');
               var button = document.getElementById(site + 'Button');
-
-              divElement.setAttribute('style', 'margin-top: 10px;');
-              userImage.setAttribute('src', json.thumbnail);
-              divElement.setAttribute('id', site + '-login-response');
-
-              divElement.appendChild(userImage);
-              divElement.appendChild(userName);
-
               button.innerHTML = 'Logged into ' + site.charAt(0).toUpperCase() + site.slice(1);
-              loginTd.appendChild(divElement); 
 
-			  // replace generic "welcome, please login" with personalized
-			  // greeting
+			  // replace generic banner with personalized greeting
 			  constructGreetingBanner(json.name);
 
 			  // store username for later
