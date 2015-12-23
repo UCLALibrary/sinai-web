a43b8c92f83635a45885296c42f76c159aa9a35c
Kevin S. Clarke <ksclarke@gmail.com>
2015-12-23 11:19:10 -0500

    Flesh out JMX config

--- src/main/scripts/startup.sh
+++ src/main/scripts/startup.sh
@@ -6,7 +6,12 @@ KEY_PASS_CONFIG="-Dsinai.key.pass=${sinai.key.pass}"
 SINAI_TEMP_DIR="-Dsinai.temp.dir=${sinai.temp.dir}"
 SINAI_PORT="-Dsinai.port=${sinai.port}"
 DROPWIZARD_METRICS="-Dvertx.metrics.options.enabled=true -Dvertx.metrics.options.registryName=sinai.metrics"
-JMX_METRICS="-Dcom.sun.management.jmxremote -Dvertx.options.jmxEnabled=true"
+JMX_METRICS="-Dcom.sun.management.jmxremote -Dvertx.metrics.options.jmxEnabled=true"
+# For tools like Eclipse's Debugging
+JDWP_AGENTLIB="-agentlib:jdwp=transport=dt_socket,address=9003,server=y,suspend=n"
+# For tools like visualvm or jconsole (Note: only for use on dev's localhost since there is no configured security)
+JMXREMOTE="-Dcom.sun.management.jmxremote.port=9001 -Dcom.sun.management.jmxremote.authenticate=false"
+JMXREMOTE="$JMXREMOTE -Dcom.sun.management.jmxremote.ssl=false"
 AUTHBIND=""
 SINAI_CONFIG=""
 
@@ -21,5 +26,15 @@ if [ -e "${sinai.json.config.path}" ]; then
   SINAI_CONFIG="-conf ${sinai.json.config.path}"
 fi
 
-$AUTHBIND java $LOG_DELEGATE $KEY_PASS_CONFIG $SINAI_TEMP_DIR $SINAI_PORT $DROPWIZARD_METRICS $1 \
-  -jar target/sinai-web-${project.version}-exec.jar $SINAI_CONFIG
+if [[ "${jmx.tool}" == *"JDWP_AGENTLIB"* ]]; then
+  echo "Using JDWP_AGENTLIB for JMX connections (port 9003)"
+  JMX_METRICS="$JMX_METRICS $JDWP_AGENTLIB"
+fi
+
+if [[ "${jmx.tool}" == *"JMX_REMOTE"* ]]; then
+  echo "Using JMX_REMOTE for JMX connections (port 9001)"
+  JMX_METRICS="$JMX_METRICS $JMX_REMOTE"
+fi
+
+$AUTHBIND java $LOG_DELEGATE $KEY_PASS_CONFIG $SINAI_TEMP_DIR $SINAI_PORT $DROPWIZARD_METRICS \
+  $JMX_METRICS $1 -jar target/sinai-web-${project.version}-exec.jar $SINAI_CONFIG
