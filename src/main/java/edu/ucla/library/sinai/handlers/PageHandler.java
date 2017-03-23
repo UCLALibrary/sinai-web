afbeb40ece1e04356d75188859b5fdcaa7b06ee1
Mark Allen Matney, Jr <markamatney@gmail.com>
2017-03-23 15:04:59 -0700

    Revert "Stage pr"

--- src/main/java/edu/ucla/library/sinai/handlers/PageHandler.java
+++ src/main/java/edu/ucla/library/sinai/handlers/PageHandler.java
@@ -2,10 +2,11 @@
 package edu.ucla.library.sinai.handlers;
 
 import static edu.ucla.library.sinai.Constants.HBS_DATA_KEY;
+import static edu.ucla.library.sinai.Constants.MANUSCRIPT_METADATA_PROP;
 import static edu.ucla.library.sinai.RoutePatterns.BROWSE;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import edu.ucla.library.sinai.Configuration;
@@ -31,11 +32,25 @@ public class PageHandler extends SinaiHandler {
     @Override
     public void handle(final RoutingContext aContext) {
         final ObjectMapper mapper = new ObjectMapper();
-        final ObjectNode jsonNode;
+        ObjectNode jsonNode;
+        String errorMessage;
 
         // If user is navigating to the browse page, need to load metadata
         if (aContext.normalisedPath().equals(BROWSE)) {
-            jsonNode = myConfig.getManuscriptMetadata();
+            try {
+                String metadataFilePath = System.getProperty(MANUSCRIPT_METADATA_PROP);
+                InputStream in = getClass().getResourceAsStream(metadataFilePath); 
+                BufferedReader metadataFile = new BufferedReader(new InputStreamReader(in));
+                jsonNode = (ObjectNode) mapper.readTree(metadataFile);
+            } catch (Exception e) {
+                errorMessage = "Something went wrong while loading the manuscript metadata. ERROR: " + e.toString();
+                if (LOGGER.isDebugEnabled()) {
+                    LOGGER.debug("{} " + errorMessage, getClass().getSimpleName());
+                }
+
+                jsonNode = mapper.createObjectNode();
+                jsonNode.put("error", errorMessage);
+            }
         } else {
             jsonNode = mapper.createObjectNode();
         }
