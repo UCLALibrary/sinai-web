6cc48e401ca7dc4b94af2f0a5ee713886fb9a594
Mark Allen Matney, Jr <mmatney@library.ucla.edu>
2018-04-13 16:26:40 -0700

    update to Mirador 2.6.0 + bugfixes

--- src/main/webapp/mirador/plugins/print/plugin.js
+++ src/main/webapp/mirador/plugins/print/plugin.js
@@ -1,32 +1,38 @@
-/**
- * plugin.js
- *
- * Copyright, Moxiecode Systems AB
- * Released under LGPL License.
- *
- * License: http://www.tinymce.com/license
- * Contributing: http://www.tinymce.com/contributing
- */
+(function () {
+var print = (function () {
+  'use strict';
 
-/*global tinymce:true */
+  var PluginManager = tinymce.util.Tools.resolve('tinymce.PluginManager');
 
-tinymce.PluginManager.add('print', function(editor) {
-	editor.addCommand('mcePrint', function() {
-		editor.getWin().print();
-	});
+  var register = function (editor) {
+    editor.addCommand('mcePrint', function () {
+      editor.getWin().print();
+    });
+  };
+  var $_dhjzmticje5o2ujd = { register: register };
 
-	editor.addButton('print', {
-		title: 'Print',
-		cmd: 'mcePrint'
-	});
+  var register$1 = function (editor) {
+    editor.addButton('print', {
+      title: 'Print',
+      cmd: 'mcePrint'
+    });
+    editor.addMenuItem('print', {
+      text: 'Print',
+      cmd: 'mcePrint',
+      icon: 'print'
+    });
+  };
+  var $_2lbm2midje5o2uje = { register: register$1 };
 
-	editor.addShortcut('Ctrl+P', '', 'mcePrint');
+  PluginManager.add('print', function (editor) {
+    $_dhjzmticje5o2ujd.register(editor);
+    $_2lbm2midje5o2uje.register(editor);
+    editor.addShortcut('Meta+P', '', 'mcePrint');
+  });
+  function Plugin () {
+  }
 
-	editor.addMenuItem('print', {
-		text: 'Print',
-		cmd: 'mcePrint',
-		icon: 'print',
-		shortcut: 'Ctrl+P',
-		context: 'file'
-	});
-});
+  return Plugin;
+
+}());
+})();
