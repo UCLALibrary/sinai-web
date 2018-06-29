b0de382ab3182affc4d4352d33005a90d451e9b5
Mark Allen Matney, Jr <mmatney@library.ucla.edu>
2018-06-29 14:18:39 -0700

    fix error in PDF route pattern
    add several PDFs to the site

--- src/main/java/edu/ucla/library/sinai/handlers/PDFProxyHandler.java
+++ src/main/java/edu/ucla/library/sinai/handlers/PDFProxyHandler.java
@@ -15,8 +15,40 @@ public class PDFProxyHandler extends SinaiHandler {
 
     @Override
     public void handle(final RoutingContext aContext) {
-        // TODO: check the normalisedPath and serve the proper PDF based on it, or return error page
-        aContext.reroute("/pdfs/test.pdf");
+        switch (aContext.normalisedPath()) {
+        case "/terms-of-use/please-read":
+            aContext.reroute("/pdfs/A-1_Terms-of-Use_20180628.pdf");
+            break;
+        case "/terms-of-use/contributors":
+            aContext.reroute("/pdfs/A-2-3_Citing-Contributors_20180628.pdf");
+            break;
+        case "/user-guide":
+            aContext.reroute("/pdfs/B-0_User-Guide-TOC_20180629-rev.pdf");
+            break;
+        case "/user-guide/mss-shelfmarks":
+            aContext.reroute("/pdfs/B-1_Manuscript-Shelfmarks_20180629.pdf");
+            break;
+        case "/user-guide/mss-terms":
+            aContext.reroute("/pdfs/B-4_Descriptions-Glossary_20180629.pdf");
+            break;
+        case "/user-guide/search-tips":
+            aContext.reroute("/pdfs/B-5_Keyword-Search-Tips_20180629.pdf");
+            break;
+        case "/user-guide/works-cited":
+            aContext.reroute("/pdfs/B-6_Works-Cited_20180628.pdf");
+            break;
+        case "/publications":
+            aContext.reroute("/pdfs/C-1_Publications_20180629.pdf");
+            break;
+        case "/contacts":
+            aContext.reroute("/pdfs/C-2_Contact-Us_20180628.pdf");
+            break;
+        case "/user-guide/navigating-site":
+        case "/user-guide/choosing-images":
+        default:
+            aContext.reroute("/pdfs/test.pdf");
+            break;
+        }
     }
 
 }
